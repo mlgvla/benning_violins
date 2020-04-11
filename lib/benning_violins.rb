36307b23196f8f0d845f64aeaff84a2b334c1363
mlgvla <mlgerardvla@gmail.com>
2020-04-11 19:05:26 -0400

    Added dependencies

--- lib/benning_violins.rb
+++ lib/benning_violins.rb
@@ -1,6 +1,13 @@
 require_relative "./benning_violins/version"
 require_relative "./benning_violins/CLI"
+require_relative "./benning_violins/inventory"
+require_relative "./benning_violins/instrument"
 require_relative "./benning_violins/scraper"
+
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
 
 module BenningViolins
   class Error < StandardError; end
