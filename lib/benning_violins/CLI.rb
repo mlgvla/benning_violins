77fa88bb5ca39989c0726256ba3154cc5c7fceaa
mlgvla <mlgerardvla@gmail.com>
2020-04-11 01:20:08 -0400

    Completed Scraping Details and conneted to CLI

--- lib/benning_violins/CLI.rb
+++ lib/benning_violins/CLI.rb
@@ -7,6 +7,7 @@ class BenningViolins::CLI
 
     def run
         make_inventory
+        add_details_to_instruments
     end
 
     def make_inventory
@@ -15,4 +16,12 @@ class BenningViolins::CLI
         puts instrument_inventory
     end
 
+    def add_details_to_instruments
+        #hard code now - eventually grab url from iterating thorough Inventory? or Instrument @@all
+        #eventually add avcategory attribute to Instrument to link it to a specific Inventory. Do that
+        #in Instrument Class
+        instrument_choice = "Fine-Violin-Catalog-Fine-Violins-for-Sale-Sergio-Peresson-violin-552.html"
+        instrument_details = BenningViolins::Scraper.scrape_details_page(BASE_PATH + instrument_choice)
+        puts instrument_details
+    end
 end
--- lib/benning_violins/scraper.rb
+++ lib/benning_violins/scraper.rb
@@ -4,13 +4,12 @@ require "pry"
 
 class BenningViolins::Scraper
 
-    
+    #Rename some of the instance variables to make some sense!
 
     def self.scrape_instruments(inventory_url)
         instrument_hash_array = []
 
         site = inventory_url
-
         doc = Nokogiri::HTML(open(site))
 
         things = doc.css("div.djc_items .djc_item")
@@ -26,8 +25,38 @@ class BenningViolins::Scraper
          instrument_hash_array
     end
 
-    def self.scrape_details_page(item_url)
-        #adds lot#, maker, price, description, and terms to Instrument Objects
+    def self.scrape_details_page(instrument_url)
+        #returns details hash to CLI (add_instrument_details)
+        #type, year, price, maker, description, terms(might eliminate category later)
+
+        instrument_details_hash = {}
+        
+        site = instrument_url
+      
+        doc = Nokogiri::HTML(open(site))
+        
+        things = doc.css("div.djc_fulltext p")
+      
+        things.each do |thing|
+            nbsp = Nokogiri::HTML("&nbsp;").text #convert HTML nbsp to text nbsp
+            old_details = thing.text.sub(/^[^:]+:\s*/, "") #delete label
+            details = old_details.sub(nbsp, "") #delete text nbsp
+           #Rename detail_one "label"
+            detail_one = thing.css("strong").text.strip #details label is in <strong> tag
+
+            if detail_one.include?("Type")
+              instrument_details_hash[:type]= details
+            elsif detail_one.include?("Year")
+              instrument_details_hash[:year]= details.to_i 
+            elsif detail_one.include?("Price")
+              instrument_details_hash[:price]= details            
+            elsif detail_one.include?("Description")
+              instrument_details_hash[:description]= details            
+            elsif detail_one.include?("Terms")
+              instrument_details_hash[:terms]= details 
+            end
+       end
+      return instrument_details_hash
     end
 end
 
