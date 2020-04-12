0bc6f8ea6a99884b2604248e4edb367b7e8eafe8
mlgvla <mlgerardvla@gmail.com>
2020-04-11 20:07:21 -0400

    Added get_user_inventory and valid_input?

--- lib/benning_violins/CLI.rb
+++ lib/benning_violins/CLI.rb
@@ -5,22 +5,38 @@ class BenningViolins::CLI
 
     BASE_PATH = "https://www.benningviolins.com/" #move into Inventory, Instrument, or Scraper Class
 
-    def run
+    def call
         puts "Welcome to Benning Violins"
         #until "exit"
-            #get_inventories
-            #list_inventories
-            #get_user_inventory
+            get_inventories
+            list_inventories
+            get_user_inventory
         #next?
-        puts "Thank you for visiting Benning Violins!"
+        #puts "Thank you for visiting Benning Violins!"
         #end
-
-        make_inventory # move this method into Inventory Class
-        add_details_to_instruments # move this method into Instrument Class
     end
 
-   
-
-    
+    def get_inventories
+        @inventories = ["vi", "vla", "cel", "bass"] # will get from Inventory.all, which will call Scraper
     end
+
+    def list_inventories
+        puts "Please select an instrument to see our current collection."
+
+        @inventories.each.with_index(1) do |inv, index|
+            puts "#{index}. #{inv}"        
+        end
+    end
+
+    def get_user_inventory
+        inventory_choice = gets.strip.to_i
+        valid_input?(inventory_choice) #call show_inventory if valid input
+    end
+
+    def valid_input?(choice)
+        binding.pry
+        choice.to_i <= @inventories.length && choice.to_i > 0
+        
+    end
+
 end
